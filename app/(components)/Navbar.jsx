ee1c3222376a107f1be9ec859899dc9c90ec4969
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-24 22:30:12 +0530

    Improved Navbar hover effect

--- app/(components)/Navbar.jsx
+++ app/(components)/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div className="absolute left-1/2 top-[10px] transform -translate-x-1/2 shadow-2xl p-2 flex justify-center items-center">
-            <div className="bg-black flex items-center py-1 px-1 gap-2 rounded-full text-white border-gray-300 border-2 shadow-2xl shadow-white/30 hover:shadow-white/80 hover:py-2 hover:px-2 transition-all ease-in-out duration-300">
+            <div className="bg-black flex items-center py-1 px-1 gap-2 rounded-full text-white border-gray-300 border-2 shadow-2xl shadow-white/30 hover:shadow-white/80 hover:scale-105 transition-all ease-in-out duration-300">
                 <Link
                     className="transition-all ease-in-out duration-200 hover:bg-white hover:text-black p-2 px-5 rounded-full text-xs"
                     href="/">
