5e2b8d1160ebafd9e288b305cae54eaff9101768
ItsMe Prince <mohammadbadshah92@gmail.com>
2025-07-10 21:11:34 +0530

    2.12.15 | Added node.js , added colorful tech stack

--- src/utility/devSkills.ts
+++ src/utility/devSkills.ts
@@ -6,16 +6,17 @@ const devSkills = [
   { name: "/html", file: "5.html.svg", fullName: "HTML" },
   { name: "/css", file: "6.css.svg", fullName: "CSS" },
   { name: "/javascript", file: "7.javascript.svg", fullName: "JavaScript" },
-  { name: "/express.js", file: "8.express.svg", fullName: "Express.js" },
-  { name: "/react.js", file: "9.react.svg", fullName: "React.js" },
-  { name: "/redux", file: "10.redux.svg", fullName: "Redux" },
-  { name: "/my-sql", file: "11.mySQL.svg", fullName: "MySQL" },
-  { name: "/mongo-db", file: "12.mongo-db.png", fullName: "MongoDB" },
-  { name: "/bootstrap", file: "13.bootstrap.svg", fullName: "Bootstrap" },
-  { name: "/tailwind-css", file: "14.tailwind.svg", fullName: "Tailwind CSS" },
-  { name: "/next.js", file: "15.next.svg", fullName: "Next.js" },
-  { name: "/typescript", file: "16.typescript.svg", fullName: "TypeScript" },
-  { name: "/electron.js", file: "17.electron.svg", fullName: "Electron.js"}
+    { name: "/node.js", file: "8.node.svg", fullName: "Node.js" },
+  { name: "/express.js", file: "9.express.svg", fullName: "Express.js" },
+  { name: "/react.js", file: "10.react.svg", fullName: "React.js" },
+  { name: "/redux", file: "11.redux.svg", fullName: "Redux" },
+  { name: "/my-sql", file: "12.mySQL.svg", fullName: "MySQL" },
+  { name: "/mongo-db", file: "13.mongo-db.png", fullName: "MongoDB" },
+  { name: "/bootstrap", file: "14.bootstrap.svg", fullName: "Bootstrap" },
+  { name: "/tailwind-css", file: "15.tailwind.svg", fullName: "Tailwind CSS" },
+  { name: "/next.js", file: "16.next.svg", fullName: "Next.js" },
+  { name: "/typescript", file: "17.typescript.svg", fullName: "TypeScript" },
+  { name: "/electron.js", file: "18.electron.svg", fullName: "Electron.js"}
 ];
 
 export default devSkills;
