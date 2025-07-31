369fe948f780624cc5f41ca233090761027f0962
ItsMe Prince <mohammadbadshah92@gmail.com>
2025-08-01 01:43:03 +0530

    2.126.62 | Updated UPI id

--- src/utility/utils.ts
+++ src/utility/utils.ts
@@ -1,5 +1,9 @@
+// Utils
 export const ProfilePicture: string = "/profile-picture/pfp1.jpg";
 export const ProjectDefaultImage: string = "/projects/default-project-img.png";
+export const UPI_ID: string = "itsmeprincekotak@yespop";
+
+// Links
 export const GitHubLink: string = "https://github.com/itsmeprinceyt";
 export const YouTubeLink: string = "https://www.youtube.com/@itsmeprinceyt";
 export const LinkedInLink: string = "https://www.linkedin.com/in/mohduvaish";
@@ -7,6 +11,10 @@ export const X_Link: string = "https://x.com/intent/follow?screen_name=itsmeprin
 export const CodingProjectsLink: string = "https://www.youtube.com/playlist?list=PLiFooJ43_R5SKCLgUOeECaypEXsUv8kEO";
 export const DiscordServer_Link: string = "https://discord.gg/HgXNs4p5cx";
 export const InstagramLink: string = "https://www.instagram.com/itsmeprinceyt";
+
+// Text
 export const NOT_WATCHED_ANY_COURSE: string = "I never really followed any course for this — I mostly relied on Google and watched a few YouTube tutorials whenever I got stuck or wasn’t sure what to do next.";
 export const GOOGLE_REMINDER: string = "You’ll definitely forget things from time to time — and that’s okay. Just Google it when you need to, but try to recall it on your own first. The longer you can hold onto it before looking it up again, the better it’ll stick.";
+
+// Controllers Utils
 export const SKILL_PAGE_LOCKDOWN: boolean = false;
