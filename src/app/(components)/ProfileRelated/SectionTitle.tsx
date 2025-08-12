2f20f60d66fad4f5a626b7cd075238b97c20b341
itsmeprinceyt <mohammadbadshah92@gmail.com>
2025-08-12 18:29:25 +0530

    2.126.83 | Changed link to whole text + image in the home section title

--- src/app/(components)/ProfileRelated/SectionTitle.tsx
+++ src/app/(components)/ProfileRelated/SectionTitle.tsx
@@ -2,17 +2,16 @@ import Link from "next/link"
 import Image from "next/image"
 export default function SectionTitle({ title, href }: { title: string, href: string }) {
     return (
-        <div className=" text-xl w-full max-w-40 flex items-start gap-2 p-2">
+        <Link href={href} className=" text-xl w-full max-w-32 md:max-w-40 flex items-start gap-2 p-2">
             {title}
-            <Link href={href} className="invert">
-                <Image
-                    src="/icons/open.svg"
-                    height={15}
-                    width={15}
-                    alt="Open"
-                />
-            </Link>
-        </div>
+            <Image
+                src="/icons/open.svg"
+                height={15}
+                width={15}
+                alt="Open"
+                className="invert"
+            />
+        </Link>
     )
 };
 
