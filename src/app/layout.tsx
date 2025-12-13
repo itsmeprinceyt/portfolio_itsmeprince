271d6b936dbe615f052df2e0f0c735dcb7c5cec8
itsmeprinceyt <mohammadbadshah92@gmail.com>
2025-12-13 19:14:48 +0530

    2.189.86 | Added snowfalll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Suspense } from "react";
-import Loader from './(components)/Components/Loader';
+import Loader from "./(components)/Components/Loader";
 import Sidebar from "./(components)/Sidebar";
+import SnowFall from "./(components)/Components/SnowFall";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://portfolio-itsmeprince.vercel.app/'),
+  metadataBase: new URL("https://portfolio-itsmeprince.vercel.app/"),
   title: "Mohd Uvaish - Portfolio | ItsMe Prince",
-  description: "Welcome to my personal portfolio! Explore my diverse skills ranging from programming and technical expertise to soft skills. Check out my meticulously crafted projects and get an idea of what I can offer.",
+  description:
+    "Welcome to my personal portfolio! Explore my diverse skills ranging from programming and technical expertise to soft skills. Check out my meticulously crafted projects and get an idea of what I can offer.",
   icons: {
     icon: "/profile-picture/pfp1.jpg",
   },
@@ -22,13 +24,12 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(80,80,80,0.4),rgba(0,0,0,0))] select-none">
         <Suspense fallback={<Loader />}>
+          <SnowFall />
           <div className="flex min-h-screen">
             <div className="min-h-screen">
               <Sidebar />
             </div>
-            <main className="flex-1">
-              {children}
-            </main>
+            <main className="flex-1">{children}</main>
           </div>
         </Suspense>
       </body>
