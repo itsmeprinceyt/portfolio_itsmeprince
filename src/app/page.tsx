18671e44e0aae542b8484f30942a5b7b0d90b8d0
itsmeprinceyt <mohammadbadshah92@gmail.com>
2026-05-12 22:03:04 +0530

    3.0.31 | Updated Site name & added more keywords + speed analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import HomePage from "./(components)/Pages/HomePage";
 
 export const metadata: Metadata = {
-  title: "Mohd Uvaish a.k.a ItsMe Prince Portfolio",
+  title: "Mohd Uvaish | ItsMe Prince - Portfolio",
   description:
     "Mohd Uvaish (ItsMe Prince) is a Full Stack Developer skilled in Next.js, TypeScript, and modern web technologies. Explore projects, skills, and professional experience.",
   alternates: {
