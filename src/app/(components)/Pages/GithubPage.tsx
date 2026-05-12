7ce196646536b6c2a0e37304e2e4e14f83fc9cbe
ItsMe Prince <mohammadbadshah92@gmail.com>
2026-05-13 00:27:37 +0530

    hotfix | padding in GitHub page

--- src/app/(components)/Pages/GithubPage.tsx
+++ src/app/(components)/Pages/GithubPage.tsx
@@ -5,7 +5,7 @@ import PageWrapper from "../PageWrapper";
 export default function GitHubPage() {
   return (
     <PageWrapper>
-      <div className="flex flex-col items-center gap-5 justify-center min-h-screen">
+      <div className="flex flex-col items-center px-6 gap-5 justify-center min-h-screen">
         <GitHubSection />
       </div>
     </PageWrapper>
